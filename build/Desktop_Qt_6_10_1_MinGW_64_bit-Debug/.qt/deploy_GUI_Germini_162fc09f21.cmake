05dd50305ff65adff88d0cc3ec673a72ab2c67b6
Armando Payan Angulo <armandopayanangulo@gmail.com>
2026-01-03 18:27:32 -0700

    Eliminados un chingo de archivos de compilacion

--- build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/deploy_GUI_Germini_162fc09f21.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/cybershibo/Desktop/QtProjects/GUI-Germini/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/GUI-Germini-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase;qtserialport")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/cybershibo/Desktop/QtProjects/GUI-Germini/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/GUI-Germini.exe"
-    GENERATE_QT_CONF
-)
